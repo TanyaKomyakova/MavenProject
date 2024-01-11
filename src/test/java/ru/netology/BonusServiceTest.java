458b359a70ff48640526e656737844c8403e57f3
TanyaKomyakova <tanya_komyakova@mail.ru>
2024-01-11 16:05:42 +0300

    initial commit

---
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void calculate() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+
+    }
+}
