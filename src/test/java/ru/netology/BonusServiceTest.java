f5c755b4c8c51e9c81d423fcdb3b1d484f85d071
TanyaKomyakova <tanya_komyakova@mail.ru>
2024-01-18 15:10:51 +0300

    Циклы параметризованные тесты - урок

--- src/test/java/ru/netology/BonusServiceTest.java
+++ src/test/java/ru/netology/BonusServiceTest.java
@@ -1,18 +1,24 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BonusServiceTest {
 
-    @Test
-    void calculate() {
+    @CsvSource(value = {
+            "'zareg',1000_60,true,30",
+    }
+
+    )
+    @ParameterizedTest
+    void shouldCalculate(String name, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
 
-        long amount = 1000_60;
-        boolean registered = true;
-        long expected = 30;
+//        long amount = 1000_60;
+//        boolean registered = true;
+//        long expected =30;
 
         long actual = service.calculate(amount, registered);
         assertEquals(expected, actual);
